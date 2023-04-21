682ce7fbfd213044c4c30e029fa02d5826d8dcfc
jslee314 <jslee_314@naver.com>
2023-04-22 06:23:45 +0900

    viewmodel 만들기

--- app/src/main/java/com/jslee314/myRabbit/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jslee314/myRabbit/ui/main/MainViewModel.kt
@@ -3,11 +3,28 @@ package com.jslee314.myRabbit.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.jslee314.myRabbit.model.Point
+import com.jslee314.myRabbit.model.Repository
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val repository: Repository) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    val allWords: LiveData<List<Point>> = repository.allPoints.asLiveData()
+
+}
+
+class WordViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
